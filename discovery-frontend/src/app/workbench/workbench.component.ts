db6a760f5f31066276b564cbc2164eac2b88124d
Young-mo Yoo <gigamadness@gmail.com>
2018-10-23 09:44:37 +0900

    Support to import file into personal database in workbench

--- discovery-frontend/src/app/workbench/workbench.component.ts
+++ discovery-frontend/src/app/workbench/workbench.component.ts
@@ -362,6 +362,8 @@ export class WorkbenchComponent extends AbstractComponent implements OnInit, OnD
 
   public hideResultButtons: boolean = false;
 
+  public importFile: boolean = false;
+
   /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
    | Constructor
    |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
@@ -1970,7 +1972,8 @@ export class WorkbenchComponent extends AbstractComponent implements OnInit, OnD
               fields: queryResult.fields,
               filePath: queryResult.filePath,
               defaultNumRows: queryResult.defaultNumRows,
-              numRows: queryResult.numRows
+              numRows: queryResult.numRows,
+              fileAbsolutePath: queryResult.fileAbsolutePath
             }
           };
           queryResultPromises.push(promise);
@@ -2003,6 +2006,7 @@ export class WorkbenchComponent extends AbstractComponent implements OnInit, OnD
             queryResult.fields = metadata.queryResult.fields;
             queryResult.data = result;
             queryResult.csvFilePath = metadata.queryResult.filePath;
+            queryResult.csvFileAbsolutePath = metadata.queryResult.fileAbsolutePath;
             queryResult.defaultNumRows = metadata.queryResult.defaultNumRows;
             queryResult.numRows = metadata.queryResult.numRows;
             tab.result = queryResult;
@@ -2811,6 +2815,10 @@ export class WorkbenchComponent extends AbstractComponent implements OnInit, OnD
 
   } // function - createDatasource
 
+  public importFileSucceed() {
+    this.detailWorkbenchDatabase.getDatabase();
+  }
+
   /**
    * 데이터소스 생성 완료
    */
@@ -3110,7 +3118,7 @@ export class WorkbenchComponent extends AbstractComponent implements OnInit, OnD
 
   public saveAsHiveTable() {
     const currentTab: ResultTab = this._getCurrentResultTab();
-    this.saveAsHiveTableComponent.init(this.workbenchId, currentTab.result.csvFilePath, this.websocketId);
+    this.saveAsHiveTableComponent.init(this.workbenchId, currentTab.result.csvFileAbsolutePath, this.websocketId);
   }
 
   public saveAsHiveTableSucceed() {
@@ -3439,6 +3447,7 @@ class ResultTab {
 class QueryResult {
   // public auditId: string;
   public csvFilePath: string;
+  public csvFileAbsolutePath: string;
   public data: any[];
   public fields: Field[];
   public numRows: number;
