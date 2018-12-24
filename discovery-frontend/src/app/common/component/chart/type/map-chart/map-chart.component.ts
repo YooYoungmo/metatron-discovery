87957548f31b96b3eb20861e9467acb9aae49d96
Yoo Youngmo <gigamadness@gmail.com>
2018-12-24 15:03:04 +0900

    #ID-CUBE-18 Add Missing Code To Tango GIS (#19)

--- discovery-frontend/src/app/common/component/chart/type/map-chart/map-chart.component.ts
+++ discovery-frontend/src/app/common/component/chart/type/map-chart/map-chart.component.ts
@@ -664,6 +664,10 @@ export class MapChartComponent extends BaseChart implements AfterViewInit {
         }
     }
 
+    // Tango GIS
+    else if( _.eq(this.getUiMapOption().style, MapLayerStyle.TANGO_GIS) ) {
+      layer = this. tangoGisLayer;
+    }
     ////////////////////////////////////////////////////////
     // Map creation
     ////////////////////////////////////////////////////////
