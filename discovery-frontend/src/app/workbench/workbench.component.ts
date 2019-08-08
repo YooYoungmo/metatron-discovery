a9c5d5fa53f646a65277ef65d89e08aea3f49b38
Young-mo Yoo <gigamadness@gmail.com>
2019-08-08 17:03:02 +0900

    #ID-CUBE-58 Hive table partitioning support when uploading files to personal database in workbench

--- discovery-frontend/src/app/workbench/workbench.component.ts
+++ discovery-frontend/src/app/workbench/workbench.component.ts
@@ -3122,7 +3122,7 @@ export class WorkbenchComponent extends AbstractComponent implements OnInit, OnD
 
   public saveAsHiveTable() {
     const currentTab: ResultTab = this._getCurrentResultTab();
-    this.saveAsHiveTableComponent.init(this.workbenchId, currentTab.result.csvFileAbsolutePath, this.websocketId);
+    this.saveAsHiveTableComponent.init(this.workbenchId, currentTab.result.csvFileAbsolutePath, this.websocketId, this.workbench.dataConnection);
   }
 
   public saveAsHiveTableSucceed() {
